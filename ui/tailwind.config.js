c265ebc945004691e19ed8014454472cfd21d0c9
johndeweyzxcv <johndewey22256@gmail.com>
2022-11-05 22:25:50 +0800

    Added Tailwind

---
+++ ui/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        laptop: {max: "970px"},
+        tablet: {max: "600px"},
+        stablet: {max: "500px"},
+      },
+      fontFamily: {
+        JetBrains: ["JetBrains mono", "sans-serif"],
+      },
+      colors: {
+        Cherry: "#dd2476",
+        Ponkan: "#ff512f",
+        ShallowGrey: "#7c7c7c",
+      },
+    },
+  },
+  plugins: [],
+};
