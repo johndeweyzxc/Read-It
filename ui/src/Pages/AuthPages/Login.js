dd7ae8cf8f23c971c2184ca511bf88bf64472c24
johndeweyzxc <johndewey02003@gmail.com>
2022-11-06 20:11:22 +0800

    Merged the LoginForm and the Login component into one file

--- ui/src/Pages/AuthPages/Login.js
+++ ui/src/Pages/AuthPages/Login.js
@@ -1,11 +1,47 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import {useNavigate} from "react-router-dom";
 
-import LoginForm from "./LoginForm";
 import {localIP} from "../../ip";
 
 export default function Login() {
   const navigate = useNavigate();
+  const username = useRef();
+  const password = useRef();
+
+  // API post request to the server to login will happen here.
+  const loginRequest = async () => {
+    const apiServerLogin = `http://${localIP}:4000/Login`;
+    let response;
+
+    try {
+      response = await fetch(apiServerLogin, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          UserName: username.current.value,
+          Password: password.current.value,
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (response) {
+      const result = await response.json();
+      const token = result.message.token;
+
+      if (response.status === 201) {
+        // Store the token in the local storage, we will use this token for some authentication
+        localStorage.setItem("tokenId", JSON.stringify(token));
+
+        navigate("/Home");
+      } else {
+        alert("Wrong username or password");
+      }
+    }
+  };
 
   const TOKEN_ID = "tokenId";
 
@@ -38,12 +74,77 @@ export default function Login() {
     );
   };
 
+  const LoginForm = () => {
+    return (
+      <div
+        className='
+          h-[85vh] w-screen flex justify-center 
+          items-center laptop:mt-8 laptop:items-start'
+      >
+        <div
+          className='p-8 border-[1px] border-solid 
+            border-ShallowGrey rounded-lg flex flex-col shadow'
+        >
+          <input
+            className='w-[25vw] mb-4 border-2 border-solid border-Ponkan
+            rounded-lg p-2 outline-none text-lg font-JetBrains laptop:w-[40vw]
+            laptop:text-base tablet:w-[60vw]'
+            name={"userName"}
+            type={"text"}
+            placeholder={"Username"}
+            ref={username}
+          />
+          <input
+            className='w-[25vw] mb-2 border-2 border-solid border-Ponkan
+              rounded-lg p-2 outline-none text-lg font-JetBrains laptop:w-[40vw]
+              laptop:text-base tablet:w-[60vw]'
+            name={"passWord"}
+            type={"password"}
+            placeholder='Password'
+            ref={password}
+          />
+
+          <div
+            className='mb-4 p-2 text-center text-base font-JetBrains
+            underline hover:cursor-pointer laptop:text-sm'
+            onClick={() => {
+              navigate("/PasswordReset");
+            }}
+          >
+            Forgot your password?
+          </div>
+          <button
+            className='w-[70%] p-2 text-base font-JetBrains font-bold tracking-wide
+              bg-Cherry border border-solid border-Cherry rounded-lg
+              text-white self-center hover:cursor-pointer shadow'
+            type={"button"}
+            onClick={loginRequest}
+          >
+            Login
+          </button>
+
+          <div className='mt-4 mb-4 w-full h-[1px] bg-ShallowGrey' />
+
+          <button
+            className='w-[70%] p-2 text-base font-JetBrains font-medium tracking-wide
+          bg-black border border-solid border-black rounded-lg
+          text-white self-center hover:cursor-pointer shadow'
+            type={"button"}
+            onClick={() => navigate("/Register")}
+          >
+            Create New Account
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className='h-screen w-screen'>
       <div className='h-[15vh] w-screen shadow-md bg-gradient-to-r from-Ponkan to-Cherry' />
       <div className='h-[85vh] w-screen flex laptop:flex-col tablet:h-[95vh]'>
         <Welcome />
-        <LoginForm localIP={localIP} />
+        <LoginForm />
       </div>
       <div className='h-[25vh] w-screen bg-gradient-to-r from-Ponkan to-Cherry' />
     </div>
