87498cca377f11a370bdc5282521bde5f88e0edf
johndeweyzxc <johndewey02003@gmail.com>
2022-11-08 11:41:54 +0800

    Moved component ForgotPassword in App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,74 @@
 import React from "react";
 import {Routes, Route, BrowserRouter as Router} from "react-router-dom";
 
-import Login from "./Pages/AuthPages/Login";
-import PasswordReset from "./Pages/AuthPages/PasswordReset";
+import "./login.css";
+import Login from "./Login";
 import Home from "./Pages/UserHome/Home";
 import User from "./Pages/UserHome/User";
 import Register from "./Pages/UserModify/Register";
 import UpdateProfile from "./Pages/UserModify/UpdateProfile";
+
+function PasswordReset() {
+  return (
+    <div className='w-screen h-screen tablet:h-auto flex flex-col justify-center'>
+      <form className='flex flex-col items-center'>
+        <div
+          className='mt-4 mb-6 pl-8 pr-8 pb-2 text-3xl phone:text-lg
+        font-JetBrains border-b-[1px] border-solid border-black'
+        >
+          Reset your Password
+        </div>
+
+        <div className='flex tablet:flex-col'>
+          <div className='inputSection'>
+            <label className='labelStyle'>Email: </label>
+            <input
+              className='inputStyle'
+              type={"text"}
+              placeholder={"johndewey22@gmail.com"}
+            />
+          </div>
+          <div className='inputSection'>
+            <label className='labelStyle'>Email OTP: </label>
+            <input
+              className='inputStyle'
+              type={"text"}
+              placeholder={"OTP Code"}
+            />
+          </div>
+        </div>
+
+        <div className='flex tablet:flex-col'>
+          <div className='inputSection'>
+            <label className='labelStyle'>Phone Number: </label>
+            <input
+              className='inputStyle'
+              type={"text"}
+              placeholder={"09993241123"}
+            />
+          </div>
+          <div className='inputSection'>
+            <label className='labelStyle'>Phone Number OTP: </label>
+            <input
+              className='inputStyle'
+              type={"text"}
+              placeholder={"OTP Code"}
+            />
+          </div>
+        </div>
+
+        <button
+          className='mt-2 mb-4 p-2 pl-8 pr-8 font-JetBrains text-base tracking-wide
+        bg-Cherry border border-solid border-Cherry rounded-lg text-white
+        hover:cursor-pointer'
+          onClick={() => alert("This feature is under development")}
+        >
+          Verify
+        </button>
+      </form>
+    </div>
+  );
+}
 
 function NotFound() {
   return <div className='m-4'>404 Error not found</div>;
