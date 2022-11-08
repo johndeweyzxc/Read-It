748b7f659a822deb26ff49fda1a55c5db7ec1f3a
johndeweyzxc <johndewey02003@gmail.com>
2022-11-08 11:45:14 +0800

    Moved Login.js from /Pages/AuthPages to /src

--- ui/src/Login.js
+++ ui/src/Login.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef} from "react";
 import {useNavigate} from "react-router-dom";
 
-import {localIP} from "../../ip";
+import {localIP} from "./ip";
 
 export default function Login() {
   const navigate = useNavigate();
