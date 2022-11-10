4dea2c62a0d5b19900a08c88611b841d7eaa44e2
johndeweyzxc <johndewey02003@gmail.com>
2022-11-10 10:12:34 +0800

    Refactored some code

--- api/server.js
+++ api/server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
-const localIP = require("./ip");
 require("dotenv").config();
 
 const login = require("./routes/Read/Login");
@@ -15,7 +14,6 @@ const userUpdate = require("./routes/Update/UserUpdate");
 const updatePost = require("./routes/Update/UpdatePost");
 const deletePost = require("./routes/Delete/DeletePost");
 
-// Default database url is mongodb://localhost:5000/ReadIt
 function connectDatabase() {
   mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
   const database = mongoose.connection;
@@ -26,26 +24,15 @@ function connectDatabase() {
 }
 
 function setupServer() {
-  const PORT = "3000";
-
-  // Localhost
-  const localHost = `http://localhost:${PORT}`;
-
-  // Local area network
-  const IP = localIP;
-  const lan = `http://${IP}:${PORT}`;
-
-  // Allow origins from the UI server
-  const UISERVER = [localHost, lan];
   app.use(
     cors({
-      origin: UISERVER,
+      origin: [process.env.ALLOWED_IP, process.env.ALLOWED_LOCALHOST],
     })
   );
 
   app.use(express.json());
 
-  app.get("/", (req, res) => {
+  app.get("/", (_, res) => {
     return res.status(404).json({message: "Unauthorized access"});
   });
 
@@ -61,14 +48,8 @@ function setupServer() {
 
   app.use("/DeletePost", deletePost);
 
-  // UI server port: 3000
-  // API server port: 4000
-  // DATABASE server port: 5000
-
   app.listen(4000, () => {
-    console.log(
-      `\nRest API Server started, listening at ${IP} on PORT ${PORT}`
-    );
+    console.log(`\nRest API Server started at ${process.env.SERVER_URL}`);
   });
 }
 
