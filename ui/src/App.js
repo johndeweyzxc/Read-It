135c182d7e573e22e3ddf3586ca5cecee6a551a6
johndeweyzxc <johndewey02003@gmail.com>
2022-11-11 20:33:20 +0800

    Refactored some code

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,66 +1,50 @@
 import React from "react";
-import {Routes, Route, BrowserRouter as Router} from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 import "./Styles/login.css";
 import Login from "./Login";
-import Home from "./Pages/UserHome/Home";
-import User from "./Pages/UserHome/User";
+import Home from "./UserHome/Home";
+import User from "./UserHome/User";
 import Register from "./Register";
 import UpdateProfile from "./UpdateProfile";
 
 function PasswordReset() {
   return (
-    <div className='w-screen h-screen tablet:h-auto flex flex-col justify-center'>
-      <form className='flex flex-col items-center'>
+    <div className="w-screen h-screen tablet:h-auto flex flex-col justify-center">
+      <form className="flex flex-col items-center">
         <div
-          className='mt-4 mb-6 pl-8 pr-8 pb-2 text-3xl phone:text-lg
-        font-JetBrains border-b-[1px] border-solid border-black'
+          className="mt-4 mb-6 pl-8 pr-8 pb-2 text-3xl phone:text-lg
+        font-JetBrains border-b-[1px] border-solid border-black"
         >
           Reset your Password
         </div>
 
-        <div className='flex tablet:flex-col'>
-          <div className='inputSection'>
-            <label className='labelStyle'>Email: </label>
-            <input
-              className='inputStyle'
-              type={"text"}
-              placeholder={"johndewey22@gmail.com"}
-            />
+        <div className="flex tablet:flex-col">
+          <div className="inputSection">
+            <label className="labelStyle">Email: </label>
+            <input className="inputStyle" type={"text"} placeholder={"johndewey22@gmail.com"} />
           </div>
-          <div className='inputSection'>
-            <label className='labelStyle'>Email OTP: </label>
-            <input
-              className='inputStyle'
-              type={"text"}
-              placeholder={"OTP Code"}
-            />
+          <div className="inputSection">
+            <label className="labelStyle">Email OTP: </label>
+            <input className="inputStyle" type={"text"} placeholder={"OTP Code"} />
           </div>
         </div>
 
-        <div className='flex tablet:flex-col'>
-          <div className='inputSection'>
-            <label className='labelStyle'>Phone Number: </label>
-            <input
-              className='inputStyle'
-              type={"text"}
-              placeholder={"09993241123"}
-            />
+        <div className="flex tablet:flex-col">
+          <div className="inputSection">
+            <label className="labelStyle">Phone Number: </label>
+            <input className="inputStyle" type={"text"} placeholder={"09993241123"} />
           </div>
-          <div className='inputSection'>
-            <label className='labelStyle'>Phone Number OTP: </label>
-            <input
-              className='inputStyle'
-              type={"text"}
-              placeholder={"OTP Code"}
-            />
+          <div className="inputSection">
+            <label className="labelStyle">Phone Number OTP: </label>
+            <input className="inputStyle" type={"text"} placeholder={"OTP Code"} />
           </div>
         </div>
 
         <button
-          className='mt-2 mb-4 p-2 pl-8 pr-8 font-JetBrains text-base tracking-wide
+          className="mt-2 mb-4 p-2 pl-8 pr-8 font-JetBrains text-base tracking-wide
         bg-Cherry border border-solid border-Cherry rounded-lg text-white
-        hover:cursor-pointer'
+        hover:cursor-pointer"
           onClick={() => alert("This feature is under development")}
         >
           Verify
@@ -71,15 +55,11 @@ function PasswordReset() {
 }
 
 function NotFound() {
-  return <div className='m-4'>404 Error not found</div>;
+  return <div className="m-4">404 Error not found</div>;
 }
 
 function ServerError() {
-  return (
-    <div className='m-4'>
-      500 Internal Server Error, contact the administrator
-    </div>
-  );
+  return <div className="m-4">500 Internal Server Error, contact the administrator</div>;
 }
 
 export default function App() {
@@ -87,21 +67,21 @@ export default function App() {
     <Router>
       <Routes>
         <>
-          <Route path='/' element={<Login />} />
-          <Route path='/Register' element={<Register />} />
-          <Route path='/PasswordReset' element={<PasswordReset />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/PasswordReset" element={<PasswordReset />} />
         </>
         <>
-          <Route path='/Home' element={<Home />} />
-          <Route path='/UpdateProfile' element={<UpdateProfile />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/UpdateProfile" element={<UpdateProfile />} />
         </>
         <>
-          <Route path='/User' element={<Login />} />
-          <Route path='/User/:Username' element={<User />} />
+          <Route path="/User" element={<Login />} />
+          <Route path="/User/:Username" element={<User />} />
         </>
         <>
-          <Route path='/ServerError' element={<ServerError />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="/ServerError" element={<ServerError />} />
+          <Route path="*" element={<NotFound />} />
         </>
       </Routes>
     </Router>
