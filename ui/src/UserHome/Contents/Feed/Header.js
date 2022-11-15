b2b8d62379d4389febcfaf65b99cf4802ffbdd38
johndeweyzxc <johndewey02003@gmail.com>
2022-11-15 17:44:47 +0800

    Converted css from styled-components into tailwind css

--- ui/src/UserHome/Contents/Feed/Header.js
+++ ui/src/UserHome/Contents/Feed/Header.js
@@ -1,120 +1,8 @@
-// Dependency imports
 import React from "react";
-import styled from "styled-components";
-// App imports
 import GlobeIcon from "../Assets/Globe-icon.png";
 import LockIcon from "../Assets/Lock-icon.png";
 import EditIcon from "../Assets/Edit-icon.png";
 import TrashIcon from "../Assets/Trash-icon.png";
-
-const HeaderDiv = styled.div`
-  padding: 0.5rem;
-  margin-bottom: 0.25rem;
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid #999999c5;
-  font-family: "JetBrains mono", monospace;
-`;
-
-const HeaderInfo = styled.div`
-  display: flex;
-  @media screen and (max-width: 720px) {
-    flex-direction: column;
-  }
-`;
-
-const HeaderUDDiv = styled(HeaderInfo)`
-  display: none;
-  align-self: center;
-  @media screen and (max-width: 420px) {
-    flex-direction: row;
-    display: flex;
-  }
-`;
-
-const HeaderIcon = styled.img`
-  margin-right: 0.5rem;
-  aspect-ratio: 1;
-  height: 1rem;
-  align-self: center;
-  @media screen and (max-width: 740px) {
-    display: none;
-  }
-`;
-
-const HeaderUDIcon = styled(HeaderIcon)`
-  display: none;
-  @media screen and (max-width: 420px) {
-    display: block;
-  }
-`;
-
-const MobileIcon = styled(HeaderIcon)`
-  align-self: center;
-  display: none;
-  @media screen and (max-width: 740px) {
-    display: block;
-  }
-`;
-
-const HeaderText = styled.div`
-  margin-right: 0.5rem;
-  color: #2525259d;
-  font-size: 0.8rem;
-  align-self: center;
-  text-decoration: ${(props) => props.textDecoration};
-  @media screen and (max-width: 720px) {
-    align-self: flex-start;
-  }
-`;
-
-const HeaderUDText = styled(HeaderText)`
-  font-size: 0.7rem;
-  display: block;
-`;
-
-const FullNameText = styled.div`
-  margin-right: 0.5rem;
-  color: #000;
-  font-size: 1rem;
-  align-self: center;
-  @media screen and (max-width: 720px) {
-    font-size: 0.8rem;
-  }
-`;
-
-const InterpunctDiv = styled.div`
-  display: flex;
-`;
-
-const Interpunct = styled.div`
-  color: #2525259d;
-  @media screen and (max-width: 740px) {
-    display: none;
-  }
-`;
-
-const MobileInterpunct = styled(Interpunct)`
-  align-self: center;
-  display: none;
-  @media screen and (max-width: 740px) {
-    display: block;
-  }
-`;
-const HeaderDateDiv = styled.div`
-  display: flex;
-  @media screen and (max-width: 420px) {
-    display: none;
-  }
-`;
-
-const HeaderDateContent = styled.div`
-  display: flex;
-  @media screen and (max-width: 600px) {
-    flex-direction: column;
-    margin-right: ${(props) => props.marginRight};
-  }
-`;
 
 export default function Header({ headerData, DeletePost, ShowEditPost }) {
   const UserName = headerData.UserName;
@@ -129,25 +17,35 @@ export default function Header({ headerData, DeletePost, ShowEditPost }) {
   // This component shows up in the header part of the feed if the screen size is less than 1000 pixel
   function HeaderUpdateDelete() {
     const headerMods = (
-      <HeaderUDDiv>
-        <HeaderUDDiv
+      <div className="hidden self-center phone:flex-row phone:flex">
+        <div
+          className="hidden self-center phone:flex-row phone:flex"
           onClick={() => {
             ShowEditPost(feedId, showPublic, feedContent);
           }}
         >
-          <HeaderUDIcon src={EditIcon} />
-          <HeaderUDText>Edit</HeaderUDText>
-        </HeaderUDDiv>
+          <img
+            className="hidden mr-2 aspect-square h-2 self-center phone:block"
+            src={EditIcon}
+            alt={"Edit this post"}
+          />
+          <div className="mr-2 text-[#2525259d] text-xs self-center tablet:self-start">Edit</div>
+        </div>
 
-        <HeaderUDDiv
+        <div
+          className="hidden self-center phone:flex-row phone:flex"
           onClick={() => {
             DeletePost(feedId);
           }}
         >
-          <HeaderUDIcon src={TrashIcon} />
-          <HeaderUDText>Delete</HeaderUDText>
-        </HeaderUDDiv>
-      </HeaderUDDiv>
+          <img
+            className="hidden mr-2 aspect-square h-2 self-center phone:block"
+            src={TrashIcon}
+            alt={"Delete this post"}
+          />
+          <div className="mr-2 text-[#2525259d] text-xs self-center tablet:self-start">Delete</div>
+        </div>
+      </div>
     );
 
     if (allowModifications) {
@@ -158,31 +56,45 @@ export default function Header({ headerData, DeletePost, ShowEditPost }) {
   }
 
   return (
-    <HeaderDiv>
-      <HeaderInfo>
-        <InterpunctDiv>
-          <FullNameText>{FullName}</FullNameText>
-          <MobileInterpunct>·</MobileInterpunct>
-          <MobileIcon src={showPublic ? GlobeIcon : LockIcon} />
-        </InterpunctDiv>
+    <div className="p-2 mr-1 flex justify-between border-b-[1px] border-solid border-[#999999c5] font-JetBrains">
+      <div className="flex tablet:flex-col">
+        <div className="flex">
+          <div className="mr-2 text-black text-base self-center tablet:text-xs">{FullName}</div>
+          <div className="text-[#2525259d] hidden tablet:block self-center">·</div>
+          <img
+            className="mr-2 aspect-square h-4 self-center hidden tablet:block"
+            src={showPublic ? GlobeIcon : LockIcon}
+            alt={"Post privacy status"}
+          />
+        </div>
 
-        <HeaderText textDecoration={"underline"}>@{UserName}</HeaderText>
-        <Interpunct>·</Interpunct>
-        <HeaderIcon src={showPublic ? GlobeIcon : LockIcon} />
-      </HeaderInfo>
+        <div className="mr-2 text-[#2525259d] text-xs self-center underline tablet:self-start">
+          @{UserName}
+        </div>
+        <div className="text-[#2525259d] tablet:hidden">·</div>
+        <img
+          className="mr-2 aspect-square h-4 self-center tablet:hidden"
+          src={showPublic ? GlobeIcon : LockIcon}
+          alt={"Post privacy status"}
+        />
+      </div>
 
-      <HeaderDateDiv>
-        <HeaderDateContent marginRight={"1rem"}>
-          <HeaderText>Posted on</HeaderText>
-          <HeaderText>{feedPostedAt}</HeaderText>
-        </HeaderDateContent>
+      <div className="flex phone:hidden">
+        <div className="flex stablet:flex-col stablet:mr-4">
+          <div className="mr-2 text-[#2525259d] text-xs self-center tablet:self-start">Posted on</div>
+          <div className="mr-2 text-[#2525259d] text-xs self-center tablet:self-start">
+            {feedPostedAt}
+          </div>
+        </div>
 
-        <HeaderDateContent>
-          <HeaderText>Edited on</HeaderText>
-          <HeaderText>{feedUpdatedAt}</HeaderText>
-        </HeaderDateContent>
-      </HeaderDateDiv>
+        <div className="flex stablet:flex-col stablet:mr-4">
+          <div className="mr-2 text-[#2525259d] text-xs self-center tablet:self-start">Edited on</div>
+          <div className="mr-2 text-[#2525259d] text-xs self-center tablet:self-start">
+            {feedUpdatedAt}
+          </div>
+        </div>
+      </div>
       <HeaderUpdateDelete />
-    </HeaderDiv>
+    </div>
   );
 }
