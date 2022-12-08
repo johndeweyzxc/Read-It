f52a4786721ec013f5d63ce2e090853a98d3e153
johndeweyzxc <johndewey02003@gmail.com>
2022-12-08 21:53:54 +0800

    Moved the Menu component for mobile in from PhoneScreen.js to Home.js

--- ui/src/UserHome/Home.js
+++ ui/src/UserHome/Home.js
@@ -1,14 +1,45 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import Switch from "@mui/material/Switch";
 
 import ApiRequest from "./ApiFunctions";
 import Header from "./Header";
 import Content from "./Content";
-import { MenuForPhone } from "./Responsive/PhoneScreen";
 import "./Home.css";
 import "./UpdatePost.css";
 import "./DeletePost.css";
+
+function MenuForPhone({ mobileMenuRef }) {
+  // This is the menu navigation for small screen devices.
+  const navigate = useNavigate();
+
+  return (
+    <div className="MobileMenuDiv" ref={mobileMenuRef}>
+      <div className="flex flex-col bg-white border-[1px] border-solid border-[#999999c5] ">
+        <Link className="MobileMenuBtn" to={"/UpdateProfile"}>
+          Update Profile
+        </Link>
+        <Link
+          className="MobileMenuBtn"
+          to={"/Home"}
+          onClick={() => {
+            localStorage.removeItem("tokenId");
+            navigate("/");
+          }}
+        >
+          Logout
+        </Link>
+
+        <button
+          className="MobileButtonBck"
+          onClick={() => (mobileMenuRef.current.style.display = "none")}
+        >
+          back
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   const TOKEN_ID = "tokenId";
